ffbfb2f5264b1219c388fd650887ed1a068d334b
AndrejNil <and.kucheroff@yandex.ru>
2023-02-02 22:02:14 +0500

    Продолжаю верстать главную страницу

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,29 +12,7 @@
         <div class="table__content">
 
 
-            <div class="table__row">
-                <div class="table__coll table__coll_name">
-                    <a href="{{route('home')}}" class="table__link">
-                        Первая задача
-                    </a>
 
-                </div>
-                <div class="table__coll">!</div>
-                <div class="table__coll">V</div>
-                <div class="table__coll">P</div>
-                <div class="table__coll">X</div>
-            </div>
-            <div class="table__row">
-                <div class="table__coll table__coll_name">
-                    <a href="{{route('home')}}" class="table__link">
-                        Первая задача
-                    </a>
-                </div>
-                <div class="table__coll">!</div>
-                <div class="table__coll">V</div>
-                <div class="table__coll">P</div>
-                <div class="table__coll">X</div>
-            </div>
             <div class="table__row">
                 <div class="table__coll table__coll_name">
                     <a href="{{route('home')}}" class="table__link">
@@ -42,23 +20,23 @@
                     </a>
                 </div>
                 <div class="table__coll">
-                    <a href="{{route('home')}}" class="table__btn btn">
-                        !
+                    <a href="{{route('home')}}" class="table__btn btn btn_yellow">
+                        <i class="table__btn-icon table__btn-icon_important" title="Важное"></i>
                     </a>
                 </div>
                 <div class="table__coll">
-                    <a href="{{route('home')}}" class="table__btn btn">
-                        V
+                    <a href="{{route('home')}}" class="table__btn btn btn_green">
+                        <i class="table__btn-icon table__btn-icon_clock" title="статус"></i>
                     </a>
                 </div>
                 <div class="table__coll">
-                    <a href="{{route('home')}}" class="table__btn btn">
-                        P
+                    <a href="{{route('home')}}" class="table__btn btn btn_blue">
+                        <i class="table__btn-icon table__btn-icon_edit" title="Редостировать"></i>
                     </a>
                 </div>
                 <div class="table__coll">
-                    <a href="{{route('home')}}" class="table__btn btn">
-                        X
+                    <a href="{{route('home')}}" class="table__btn btn btn_red">
+                        <i class="table__btn-icon table__btn-icon_delete" title="удалить"></i>
                     </a>
                 </div>
             </div>
